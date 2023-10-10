bd0687f275c0571268fa2e45491851c632883189
SethBowman <brandonsethbowman@gmail.com>
2023-10-10 10:04:27 -0500

    Add project files.

---
+++ StaticExample/Classroom.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExample
+{
+    public class Classroom
+    {
+        public static string Subject { get; set; } = "C#";
+
+        public int RoomNumber { get; set; }
+    }
+}
---
+++ StaticExample/Program.cs
@@ -0,0 +1,14 @@
+﻿using StaticExample;
+
+var classOne = new Classroom();
+
+classOne.RoomNumber = 1;
+
+var classTwo = new Classroom();
+
+classTwo.RoomNumber = 2;
+
+Classroom.Subject = "Java";
+
+Console.WriteLine($"Class 1 is in room {classOne.RoomNumber} and Class 2 is in room {classTwo.RoomNumber} and they teach {Classroom.Subject}");
+
